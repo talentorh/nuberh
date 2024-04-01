f567c10bca904b4b3419754b7c0c82ed49d4e3ac
EquipoDELLingeBeto <talentorh@hraei.gob.mx>
2024-04-01 12:56:00 -0600

    carga de id de empleado de mando

--- admin_class.php
+++ admin_class.php
@@ -41,7 +41,7 @@ Class Action {
 		$data .= ", parent_id ='".$parent_id."' ";
 		if(empty($id)){
 			include 'db_connect.php';
-if(isset($_SESSION['usuarioAdminRh'])) { $usernameSesion = $_SESSION['usuarioAdminRh']; }else if(isset($_SESSION['usuarioDatos'])) { $usernameSesion = $_SESSION['usuarioDatos']; }
+if(isset($_SESSION['usuarioAdminRh'])) { $usernameSesion = $_SESSION['usuarioAdminRh']; }else if(isset($_SESSION['usuarioDatos'])) { $usernameSesion = $_SESSION['usuarioDatos']; }else if(isset($_SESSION['usuarioJefe'])) { $usernameSesion = $_SESSION['usuarioJefe']; }
 $sql = $conexion->query("SELECT Empleado from plantillahraei where correo = '$usernameSesion'");
 	$row = mysqli_fetch_assoc($sql);
 $id_empleado = $row['Empleado'];
